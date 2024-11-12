5728b1c2a74459f389092064003c6168b711cbce
franciscomj0901 <franciscomj0901@gmail.com>
2024-11-12 14:40:52 +0100

    12-11-2024-M

--- src/Bloque2/Actividad2_7/HiloContador.java
+++ src/Bloque2/Actividad2_7/HiloContador.java
@@ -1,13 +1,18 @@
 package Bloque2.Actividad2_7;
 
 public class HiloContador extends Thread{
-    private static int contador = 0;
-    public HiloContador(String nombre){
+    private static int contador ;
+    public HiloContador(String nombre, int contador){
         super(nombre);
+        this.contador = contador;
     }
     public void run(){
-        contador++;
-        System.out.println(getName()+": "+contador);
+        if (contador < 5000){
+            contador++;
+            System.out.println(getName()+": "+contador);
+        }else {
+            interrupt();
+        }
     }
     public static int getContador(){
         return contador;
--- src/Bloque2/Actividad2_7/Main.java
+++ src/Bloque2/Actividad2_7/Main.java
@@ -2,20 +2,18 @@ package Bloque2.Actividad2_7;
 
 public class Main {
     public static void main(String[] args) {
-        HiloContador h1 = new HiloContador("Hilo 1");
-        HiloContador h2 = new HiloContador("Hilo 2");
-        HiloContador h3 = new HiloContador("Hilo 3");
-        HiloContador h4 = new HiloContador("Hilo 4");
-        HiloContador h5 = new HiloContador("Hilo 5");
+        Integer contador = 0;
+        HiloContador h1 = new HiloContador("Hilo 1",contador);
+        HiloContador h2 = new HiloContador("Hilo 2",contador);
+        HiloContador h3 = new HiloContador("Hilo 3",contador);
+        HiloContador h4 = new HiloContador("Hilo 4",contador);
+        HiloContador h5 = new HiloContador("Hilo 5",contador);
 
         /* Atributo que se pasa por parametros y se ioncrementa, en el run se comprueba si es 5000*/
-
-        for (int i = 0; i < 5000; i++) {
-            h1.run();
-            h2.run();
-            h3.run();
-            h4.run();
-            h5.run();
+        while (true) {
+            h1.run(); h2.run(); h3.run(); h4.run(); h5.run();
         }
+
+        System.
     }
 }
